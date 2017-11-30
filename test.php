5c4af5c970410da8085dafdfd631f14e84e2c984
Lars D-King <larztheloser@gmail.com>
2017-11-30 17:00:38 +1300

    c

---
+++ test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title>LD40 - Team7</title>
+<style>
+html, body { height: 100%; margin: 0; padding: 0; overflow: hidden; text-align: center; background-color: #000;color: #fff; font-family: sans-serif; -moz-user-select: none; -webkit-user-select: none; -ms-user-select:none; user-select:none;-o-user-select:none; }
+</style>
+</head>
+<body oncontextmenu="return false;" unselectable="on" onselectstart="return false;">
+<script src="game.js?v=1"></script>
+</body>
+</html>
